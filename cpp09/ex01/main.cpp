da8fc1ede436c8e7351c36666cc0702ff8e8fb22
ft_lisa <lisa.martin.lm02@gmail.com>
2025-07-08 11:26:41 +0200

    cpp09/ex01 finish

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,12 +1,14 @@
 #include "RPN.hpp"
 
+
+// est ce que chiffre peut etre negatif
+// comment on fait pour division par 0
 int main(int argc, char** argv)
 {
         if (argc != 2)
-                return std::cout << "error" << std::endl, 1;
+                return std::cerr << ERR_ARG << std::endl, 1;
         if(check_expression(argv[1]) == 0)
                 return 1;
-        std::cout << "resultat : " << calculated(argv[1]) << std::endl;
-        // fill_stack(argv[1]);
+        std::cout << calculated(argv[1]) << std::endl;
         return 0;
 }
